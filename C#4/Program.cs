69de3370858f0f1fda09558da136f5577d3ee8a3
Sergei Vasiliev <9670105747@mail.ru>
2023-01-30 10:22:58 +0700

    Задача на поиск элемента в заданном массиве

---
+++ C#4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Поиск элемента в заданном массиве 
+//               0  1   2   3    4   5  6   7  8  
+int[] array = { 7, 16, 38, 41, 76, 69, 7, 89, 76 };
+
+int n = array.Length;
+int find = 76;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+}
